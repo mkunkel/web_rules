78c6d3d34e468afc1c0f8fc3008fdc52b408a826
Mike Kunkel <mike@mkunkel.com>
2014-03-20 10:09:24 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'coffee-rails', '~> 4.0.0'
+gem 'haml-rails'
+gem 'jbuilder', '~> 1.2'
+gem 'jquery-rails'
+gem 'pg'
+gem 'rails', '4.0.3'
+gem 'sass-rails', '~> 4.0.0'
+gem 'sqlite3'
+gem 'turbolinks'
+gem 'uglifier', '>= 1.3.0'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :test, :development do
+  gem 'cucumber-rails', require: false
+  gem 'database_cleaner', git: 'https://github.com/bmabey/database_cleaner.git'
+  gem 'nyan-cat-formatter'
+  gem 'pry'
+  gem 'rspec'
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebRules::Application.config.secret_key_base = '6c152fdcaaa26577cfbff9cb4b4ef7c722f6bb2640d1b832b38c61cfe4c420b0bb6717f0bbcc473805a8030b7bd46170ead695ccf8f1b5f0449d549028d8b0c9'
